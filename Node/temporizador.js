a0cd09fdf47442b60eb0795789cb1225b7941b22
Gustavo Santos <72157670+GustavoRs12@users.noreply.github.com>
2022-10-18 13:15:17 -0300

    18/10/2022

--- Node/temporizador.js
+++ Node/temporizador.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule')
 
-const tarefa1 = schedule.scheduleJob('*/5 * 21 * * *', function (){
+const tarefa1 = schedule.scheduleJob('*/5 * * * * *', function (){
     console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
 
@@ -12,7 +12,7 @@ setTimeout(function () {
 // Recorente
 const regra = new schedule.RecurrenceRule()
 regra.dayOfWeek = [new schedule.Range(1,5)]
-regra.hour = 21
+regra.hour = 12
 regra.second = 30
 
 const tarefa2 = schedule.scheduleJob(regra, function () {
